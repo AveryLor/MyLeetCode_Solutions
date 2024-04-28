f338d5c26f06081e95174859e7db27f9f92c40a6
Avery Lor <averywlor@gmail.com>
2024-04-28 12:45:53 -0400

    Fixed the solution.

--- leetCodeMedium/3.LongestSubstringWithoutRepeatingCharacter/longestSubstringWithoutRepeatingcharacterSol2.py
+++ leetCodeMedium/3.LongestSubstringWithoutRepeatingCharacter/longestSubstringWithoutRepeatingcharacterSol2.py
@@ -3,7 +3,7 @@ class Solution:
         arr = []
         ans = 0
 
-        for cur in arr: 
+        for cur in s: 
             if cur not in arr: 
                 arr.append(cur)
                 ans = max(ans, len(arr)) # The max length between the last answer (last substring, and the current)
@@ -11,4 +11,4 @@ class Solution:
                 for j in range(arr.index(cur)+1): # This effectively resets the array and allows it  
                     arr.pop(0) # To set back up after finding something that was just in the array
                 arr.append(cur)
-            return ans 
+        return ans 
