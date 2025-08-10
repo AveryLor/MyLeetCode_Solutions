bdfe6a9b1c0ba32c79469379028831f282cd5422
AveryLor <averywlor@gmail.com>
2025-08-09 23:24:58 -0400

    Added 2390

---
+++ leetCodeMedium/2390. Removing Stars From a String/soln.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeStars(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []
+
+        for i in range(len(s)):
+            if s[i] == "*":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(s[i])
+        
+        return ''.join(stack)
